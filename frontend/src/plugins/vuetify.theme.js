039638f6d30f5e809e5135fa9712fea20ab91cd0
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-03-01 09:49:48 +0100

    fix vuetify theme

--- frontend/src/plugins/vuetify.theme.js
+++ frontend/src/plugins/vuetify.theme.js
@@ -6,33 +6,40 @@
 
 import colors from 'vuetify/lib/util/colors'
 
-const theme = {
-  themes: {
-    light: {
-      primary: '#0b8062',
-      anchor: '#0b8062',
-      'main-background': colors.grey.darken3,
-      'main-navigation-title': colors.shades.white,
-      'toolbar-background': '#0b8062',
-      'toolbar-title': colors.shades.white,
-      'action-button': colors.grey.darken4
-    },
-    dark: {
-      primary: '#0b8062',
-      anchor: '#0b8062',
-      'main-background': colors.grey.darken3,
-      'main-navigation-title': colors.shades.white,
-      'toolbar-background': '#0b8062',
-      'toolbar-title': colors.shades.white,
-      'action-button': colors.grey.lighten4,
-      error: colors.red.darken4,
-      warning: colors.orange.darken4
-    }
-  },
+const light = {
   dark: false,
-  options: {
-    customProperties: true,
-    variations: true
+  colors: {
+    primary: '#0b8062',
+    anchor: '#0b8062',
+    'main-background': colors.grey.darken3,
+    'main-navigation-title': colors.shades.white,
+    'toolbar-background': '#0b8062',
+    'toolbar-title': colors.shades.white,
+    'action-button': colors.grey.darken4
+  }
+}
+
+const dark = {
+  dark: true,
+  colors: {
+    primary: '#0b8062',
+    anchor: '#0b8062',
+    'main-background': colors.grey.darken3,
+    'main-navigation-title': colors.shades.white,
+    'toolbar-background': '#0b8062',
+    'toolbar-title': colors.shades.white,
+    'action-button': colors.grey.lighten4,
+    error: colors.red.darken4,
+    warning: colors.orange.darken4
+  }
+}
+
+const theme = {
+  themes: { light, dark },
+  variations: {
+    colors: ['primary', 'secondary', 'main-background'],
+    lighten: 2,
+    darken: 2,
   }
 }
 
