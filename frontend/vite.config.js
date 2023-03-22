77c30a345e5947422de908792c473e35ae6e76fd
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-03-22 09:48:27 +0100

    add SPDX file header/exclude pnp.loader.mjs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,8 @@
+//
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import path from 'path'
 import zlib from 'zlib'
 
