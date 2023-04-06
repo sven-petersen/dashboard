36687c5e33f4d86a6a0ab3a6255fe6b7e8705034
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-04-06 13:54:20 +0200

    migrate GMessage to composition API

--- frontend/src/components/GMessage.vue
+++ frontend/src/components/GMessage.vue
@@ -5,71 +5,82 @@ SPDX-License-Identifier: Apache-2.0
 -->
 
 <template>
-  <!-- TODO: previously there were a "dark"-attributes on v-alert and v-btn.
-      check if the theme-provider behaves identical -->
-  <v-theme-provider theme="dark">
-    <v-alert :color="color" :rounded="tile && '0'" closable v-model="alertVisible">
+  <v-theme-provider :theme="defaultedTheme">
+    <v-alert
+      v-model="alertVisible"
+      :color="color"
+      :rounded="tile && '0'"
+      closable
+    >
       <div class="text-subtitle-1">
-        {{message}}
-        <v-btn variant="outlined" size="small" v-if="!!detailedMessage" @click="detailedMessageVisible = !detailedMessageVisible">
+        {{ message }}
+        <v-btn
+          v-if="!!props.detailedMessage"
+          variant="outlined"
+          size="small"
+          @click="detailedMessageVisible = !detailedMessageVisible"
+        >
           Details
         </v-btn>
       </div>
       <transition name="fade">
         <div v-if="!!detailedMessageVisible">
-          <code>{{detailedMessage}}</code>
+          <code>{{ detailedMessage }}</code>
         </div>
       </transition>
     </v-alert>
   </v-theme-provider>
 </template>
 
-<script>
-export default {
-  props: {
-    message: {
-      type: String,
-      default: ''
-    },
-    detailedMessage: {
-      type: String,
-      default: ''
-    },
-    color: {
-      type: String,
-      required: true
-    },
-    tile: {
-      type: Boolean
-    },
-    dark: {
-      type: Boolean,
-      default: true
-    }
+<script setup>
+import { ref, computed, inject } from 'vue'
+
+const { theme } = inject('vuetify')
+
+const props = defineProps({
+  message: {
+    type: String,
+    default: '',
   },
-  emits: [
-    'update:message',
-    'update:detailed-message'
-  ],
-  data () {
-    return {
-      detailedMessageVisible: false
-    }
+  detailedMessage: {
+    type: String,
+    default: '',
   },
-  computed: {
-    alertVisible: {
-      get () {
-        return !!this.message
-      },
-      set (value) {
-        if (!value) {
-          this.$emit('update:message', undefined)
-          this.$emit('update:detailed-message', undefined)
-        }
-      }
-    }
+  color: {
+    type: String,
+    required: true,
+  },
+  tile: {
+    type: Boolean,
+  },
+  dark: {
+    type: Boolean,
+    default: null,
+  },
+})
+
+const emits = defineEmits([
+  'update:message',
+  'update:detailed-message',
+])
+
+const detailedMessageVisible = ref(false)
+
+const alertVisible = computed({
+  get: () => !!props.message,
+  set: (value) => {
+    if (!value) return
+    emits('update:message', undefined)
+    emits('update:detailed-message', undefined)
+  },
+})
+
+const defaultedTheme = computed(() => {
+  if (props.dark !== null) {
+    return props.dark ? 'dark' : 'light'
   }
-}
+  return theme.name.value
+})
 </script>
 
 <style lang="scss" scoped>
@@ -78,7 +89,7 @@ export default {
     transition: opacity .5s;
   }
   .fade-enter-from,
-  .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  .fade-leave-to {
     opacity: 0;
   }
 </style>
