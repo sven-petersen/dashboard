2bec1ae45f4cf7f17293f41378444a661c59d2c7
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-04-11 15:51:05 +0200

    migrate AccountAvatar to composition API

--- frontend/src/components/AccountAvatar.vue
+++ frontend/src/components/AccountAvatar.vue
@@ -7,39 +7,46 @@ SPDX-License-Identifier: Apache-2.0
 <template>
   <div class="nowrap">
     <v-avatar :size="size">
-      <img :src="avatarUrl" :alt="`avatar of ${accountName}`"/>
+      <v-img
+        :src="avatarUrl"
+        :alt="`avatar of ${accountName}`"
+      />
     </v-avatar>
-    <a v-if="mailTo && isEmail" :href="`mailto:${accountName}`" class="pl-2">{{accountName}}</a>
-    <span v-else-if="accountName" class="pl-2">{{accountName}}</span>
-    <span v-else class="pl-2 font-weight-light text--disabled">Unknown</span>
+    <a
+      v-if="mailTo && isValidMail"
+      :href="`mailto:${accountName}`"
+      class="pl-2"
+    >{{ accountName }}</a>
+    <span
+      v-else-if="accountName"
+      class="pl-2"
+    >{{ accountName }}</span>
+    <span
+      v-else
+      class="pl-2 font-weight-light text--disabled"
+    >Unknown</span>
   </div>
 </template>
 
-<script>
+<script setup>
+import { computed } from 'vue'
 import { gravatarUrlGeneric, isEmail } from '@/utils'
 
-export default {
-  props: {
-    accountName: {
-      type: String,
-      default: ''
-    },
-    mailTo: {
-      type: Boolean,
-      default: false
-    },
-    size: {
-      type: Number,
-      default: 24
-    }
+const props = defineProps({
+  accountName: {
+    type: String,
+    default: '',
   },
-  computed: {
-    avatarUrl () {
-      return gravatarUrlGeneric(this.accountName, this.size * 2)
-    },
-    isEmail () {
-      return isEmail(this.accountName)
-    }
-  }
-}
+  mailTo: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: Number,
+    default: 24,
+  },
+})
+
+const avatarUrl = computed(() => gravatarUrlGeneric(props.accountName, props.size * 2))
+const isValidMail = computed(() => isEmail(props.accountName))
 </script>
