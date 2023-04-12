cf27b5d0ad7748bc03e7e162263c36e9856f6e09
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-04-12 17:33:21 +0200

    add missing SPDX headers

--- frontend/src/utils/clock.js
+++ frontend/src/utils/clock.js
@@ -1,3 +1,9 @@
+//
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
 class Clock extends EventTarget {
   constructor (updateInterval) {
     super()
