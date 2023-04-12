aae33b477d6a037f84eea1e6af12168e5d7a8a79
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-04-12 17:32:13 +0200

    migrate components/ShootDetails/TerminalShotcutsTile to composition API

--- frontend/src/components/ShootDetails/TerminalShortcutsTile.vue
+++ frontend/src/components/ShootDetails/TerminalShortcutsTile.vue
@@ -6,118 +6,134 @@ SPDX-License-Identifier: Apache-2.0
 
 <template>
   <div>
-    <v-list-item>
-      <v-list-item-icon>
-        <icon-base width="24" height="23" icon-color="primary" view-box="-4 0 56 54">
-          <terminal-shortcut-icon></terminal-shortcut-icon>
+    <v-list-item
+      title="Terminal Shortcuts"
+      subtitle="Launch preconfigured terminals for frequently used views"
+    >
+      <template #prepend>
+        <icon-base
+          class="icon"
+          width="24"
+          height="23"
+          icon-color="primary"
+          view-box="-4 0 56 54"
+        >
+          <terminal-shortcut-icon />
         </icon-base>
-      </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-title>Terminal Shortcuts</v-list-item-title>
-        <v-list-item-subtitle>Launch preconfigured terminals for frequently used views</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action class="mx-0">
+      </template>
+
+      <template #append>
         <v-tooltip location="top">
-          <template v-slot:activator="{ on }">
-            <v-btn color="action-button" v-on="on" icon @click.stop="expansionPanel = !expansionPanel">
-              <v-icon>{{expansionPanelIcon}}</v-icon>
+          <template #activator="slotProps">
+            <v-btn
+              color="action-button"
+              icon
+              variant="text"
+              v-bind="slotProps.props"
+              @click.stop="expansionPanel = !expansionPanel"
+            >
+              <v-icon>{{ expansionPanelIcon }}</v-icon>
             </v-btn>
           </template>
-          <span>{{expansionPanelTooltip}}</span>
+          <span>{{ expansionPanelTooltip }}</span>
         </v-tooltip>
-      </v-list-item-action>
+      </template>
     </v-list-item>
     <v-expand-transition appear>
-      <v-card v-if="expansionPanel" flat class="ml-14 mt-2">
+      <v-card
+        v-if="expansionPanel"
+        flat
+        class="ml-16 mt-2 mr-1"
+      >
         <v-card-text class="pa-0">
           <terminal-shortcuts
             :shoot-item="shootItem"
-            :popper-boundaries-selector="popperBoundariesSelector"
+            :popper-boundaries-selector="props.popperBoundariesSelector"
             @add-terminal-shortcut="onAddTerminalShortcut"
-          ></terminal-shortcuts>
+          />
         </v-card-text>
       </v-card>
     </v-expand-transition>
     <unverified-terminal-shortcuts-dialog
       ref="unverified"
-    ></unverified-terminal-shortcuts-dialog>
+    />
     <webterminal-service-account-dialog
-      :namespace="shootNamespace"
       ref="serviceAccount"
-    ></webterminal-service-account-dialog>
+      :namespace="shootNamespace"
+    />
   </div>
 </template>
 
-<script>
-
-import { mapGetters } from 'vuex'
+<script setup>
+import { toRef, computed, ref } from 'vue'
+import { useStore } from 'vuex'
 import TerminalShortcuts from '@/components/TerminalShortcuts.vue'
 import IconBase from '@/components/icons/IconBase.vue'
 import TerminalShortcutIcon from '@/components/icons/TerminalShortcutIcon.vue'
 import UnverifiedTerminalShortcutsDialog from '@/components/dialogs/UnverifiedTerminalShortcutsDialog.vue'
 import WebterminalServiceAccountDialog from '@/components/dialogs/WebterminalServiceAccountDialog.vue'
 import { TargetEnum } from '@/utils'
-import { shootItem } from '@/mixins/shootItem'
 import { getMembers } from '@/utils/api'
-import get from 'lodash/get'
-import find from 'lodash/find'
 import includes from 'lodash/includes'
+import useShootItem from '@/composables/useShootItem'
 
-export default {
-  mixins: [shootItem],
-  props: {
-    popperBoundariesSelector: {
-      type: String
-    }
+const props = defineProps({
+  popperBoundariesSelector: {
+    type: String,
+    default: undefined,
   },
-  data () {
-    return {
-      expansionPanel: false
-    }
+  shootItem: {
+    type: Object,
+    required: true,
   },
-  components: {
-    TerminalShortcuts,
-    IconBase,
-    TerminalShortcutIcon,
-    UnverifiedTerminalShortcutsDialog,
-    WebterminalServiceAccountDialog
-  },
-  computed: {
-    ...mapGetters([
-      'isAdmin'
-    ]),
-    expansionPanelIcon () {
-      return this.expansionPanel ? 'mdi-chevron-up' : 'mdi-chevron-down'
-    },
-    expansionPanelTooltip () {
-      return this.expansionPanel ? 'Hide terminal shortcuts' : 'Show terminal shortcuts'
-    }
-  },
-  methods: {
-    async onAddTerminalShortcut (shortcut) {
-      if (shortcut.unverified) {
-        const confirmation = await this.$refs.unverified.promptForConfirmation()
-        if (!confirmation) {
-          return
-        }
-      }
+})
 
-      const isGardenTarget = get(shortcut, 'target') === TargetEnum.GARDEN
-      if (!this.isAdmin && isGardenTarget) {
-        const { data: projectMembers } = await getMembers({ namespace: this.shootNamespace })
-        const serviceAccountName = `system:serviceaccount:${this.shootNamespace}:dashboard-webterminal`
-        const member = find(projectMembers, ['username', serviceAccountName])
-        const roles = get(member, 'roles')
-        if (!includes(roles, 'admin')) {
-          const confirmation = await this.$refs.serviceAccount.promptForConfirmation(member)
-          if (!confirmation) {
-            return
-          }
-        }
-      }
+const emits = defineEmits(['add-terminal-shortcut'])
 
-      this.$emit('add-terminal-shortcut', shortcut)
+const {
+  shootNamespace,
+} = useShootItem(toRef(props, 'shootItem'))
+const store = useStore()
+
+const expansionPanel = ref(false)
+const serviceAccount = ref()
+
+const isAdmin = computed(() => store.getters.isAdmin)
+const expansionPanelIcon = computed(() => {
+  return expansionPanel.value ? 'mdi-chevron-up' : 'mdi-chevron-down'
+})
+const expansionPanelTooltip = computed(() => {
+  return expansionPanel.value ? 'Hide terminal shortcuts' : 'Show terminal shortcuts'
+})
+
+const onAddTerminalShortcut = async (shortcut) => {
+  if (shortcut.unverified) {
+    const confirmation = await this.$refs.unverified.promptForConfirmation()
+    if (!confirmation) {
+      return
     }
   }
+
+  const isGardenTarget = shortcut.target === TargetEnum.GARDEN
+  if (!isAdmin.value && isGardenTarget) {
+    const { data: projectMembers } = await getMembers({ namespace: shootNamespace })
+    const serviceAccountName = `system:serviceaccount:${shootNamespace}:dashboard-webterminal`
+    const member = projectMembers.find(({ username }) => username === serviceAccountName)
+    const roles = member.roles
+    if (!includes(roles, 'admin')) {
+      const confirmation = await serviceAccount.value.promptForConfirmation(member)
+      if (!confirmation) {
+        return
+      }
+    }
+  }
+
+  emits('add-terminal-shortcut', shortcut)
 }
 </script>
+
+<style lang="scss" scoped>
+.icon {
+  margin-inline-end: 32px;
+}
+</style>
