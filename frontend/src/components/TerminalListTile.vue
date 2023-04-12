2437b792ffade4380d5933f1b45f98f36cc2ba1a
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-04-12 16:56:02 +0200

    migrate components/TerminalListTile to composition API

--- frontend/src/components/TerminalListTile.vue
+++ frontend/src/components/TerminalListTile.vue
@@ -5,59 +5,70 @@ SPDX-License-Identifier: Apache-2.0
 -->
 
 <template>
-  <v-list-item>
-    <v-list-item-icon>
-      <v-icon color="primary">mdi-console</v-icon>
-    </v-list-item-icon>
-    <v-list-item-content>
-      <v-list-item-title>Terminal</v-list-item-title>
-      <v-list-item-subtitle class="pt-1">{{description}}</v-list-item-subtitle>
-    </v-list-item-content>
-    <v-list-item-action>
+  <v-list-item
+    title="Terminal"
+    :description="description"
+  >
+    <template #prepend>
+      <v-icon color="primary">
+        mdi-console
+      </v-icon>
+    </template>
+    <template #append>
       <v-tooltip location="top">
-        <template v-slot:activator="{ on }">
-          <div v-on="on">
-            <v-btn icon :to=to :disabled="disabled" color="action-button">
+        <template #activator="slotProps">
+          <div v-bind="slotProps.props">
+            <v-btn
+              icon
+              variant="text"
+              :to="to"
+              :disabled="disabled"
+              color="action-button"
+            >
               <v-icon>mdi-console-line</v-icon>
             </v-btn>
           </div>
         </template>
-        <span>{{buttonDescription || description}}</span>
+        <span>{{ props.buttonDescription || props.description }}</span>
       </v-tooltip>
-    </v-list-item-action>
+    </template>
   </v-list-item>
 </template>
 
-<script>
-import { shootItem } from '@/mixins/shootItem'
+<script setup>
+import { toRef, computed } from 'vue'
+import useShootItem from '@/composables/useShootItem'
 
-export default {
-  mixins: [shootItem],
-  props: {
-    target: {
-      type: String
-    },
-    description: {
-      type: String
-    },
-    buttonDescription: {
-      type: String
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    }
+const props = defineProps({
+  description: {
+    type: String,
+    required: true,
   },
-  computed: {
-    to () {
-      return {
-        name: 'ShootItemTerminal',
-        params: {
-          namespace: this.shootNamespace,
-          name: this.shootName
-        }
-      }
-    }
-  }
+  buttonDescription: {
+    type: String,
+    default: null,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  shootItem: {
+    type: Object,
+    required: true,
+  },
+})
+
+const {
+  shootNamespace,
+  shootName,
+} = useShootItem(toRef(props, 'shootItem'))
+
+const to = computed(() => ({
+  name: 'ShootItemTerminal',
+  params: {
+    namespace: shootNamespace,
+    name: shootName,
+  },
 }
+))
 </script>
