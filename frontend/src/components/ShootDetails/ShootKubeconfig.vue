b4a9ab06c762add61b505acda75fa60037ba92c8
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2023-04-14 16:46:17 +0200

    migrate components/ShootDetails/ShootKubeconfig to composition API

--- frontend/src/components/ShootDetails/ShootKubeconfig.vue
+++ frontend/src/components/ShootDetails/ShootKubeconfig.vue
@@ -7,134 +7,166 @@ SPDX-License-Identifier: Apache-2.0
 <template>
   <div>
     <v-list-item>
-      <v-list-item-icon>
-        <v-icon color="primary">{{icon}}</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content v-if="isGardenloginType">
-        <v-list-item-title>Kubeconfig - Gardenlogin</v-list-item-title>
-        <v-list-item-subtitle v-if="isKubeconfigAvailable" class="wrap-text">Does not contain credentials (requires <span class="font-family-monospace">gardenlogin</span> kubectl plugin)</v-list-item-subtitle>
-        <v-list-item-subtitle v-else>Gardenlogin kubeconfig currently not available</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-content v-else>
+      <template #prepend>
+        <v-icon color="primary">
+          {{ icon }}
+        </v-icon>
+      </template>
+      <template v-if="isGardenloginType">
+        <v-list-item-title>
+          Kubeconfig - Gardenlogin
+        </v-list-item-title>
+        <v-list-item-subtitle
+          v-if="isKubeconfigAvailable"
+          class="wrap-text"
+        >
+          Does not contain credentials (requires <span class="font-family-monospace">gardenlogin</span> kubectl plugin)
+        </v-list-item-subtitle>
+        <v-list-item-subtitle v-else>
+          Gardenlogin kubeconfig currently not available
+        </v-list-item-subtitle>
+      </template>
+      <template v-else>
         <v-list-item-title>Kubeconfig - Static Token</v-list-item-title>
-        <v-list-item-subtitle v-if="!shootEnableStaticTokenKubeconfig">Static token kubeconfig is disabled for this cluster</v-list-item-subtitle>
-        <v-list-item-subtitle v-else-if="!isKubeconfigAvailable">Static token kubeconfig currently not available</v-list-item-subtitle>
-        <v-list-item-subtitle class="wrap-text" v-else>Contains static token credential. Not recommended, consider disabling the static token kubeconfig</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action v-if="isGardenloginType" class="mx-0">
-        <gardenlogin-info></gardenlogin-info>
-      </v-list-item-action>
-      <v-list-item-action class="mx-0" v-if="isKubeconfigAvailable">
-        <v-tooltip location="top">
-          <template v-slot:activator="{ on }">
-            <v-btn v-on="on" icon @click.stop="onDownload" color="action-button">
+        <v-list-item-subtitle v-if="!shootEnableStaticTokenKubeconfig">
+          Static token kubeconfig is disabled for this cluster
+        </v-list-item-subtitle>
+        <v-list-item-subtitle v-else-if="!isKubeconfigAvailable">
+          Static token kubeconfig currently not available
+        </v-list-item-subtitle>
+        <v-list-item-subtitle
+          v-else
+          class="wrap-text"
+        >
+          Contains static token credential. Not recommended, consider disabling the static token kubeconfig
+        </v-list-item-subtitle>
+      </template>
+
+      <template #append>
+        <gardenlogin-info v-if="isGardenloginType" />
+        <v-tooltip
+          v-if="isKubeconfigAvailable"
+          location="top"
+        >
+          <template #activator="slotProps">
+            <v-btn
+              icon
+              variant="text"
+              color="action-button"
+              v-bind="slotProps.props"
+              @click.stop="onDownload"
+            >
               <v-icon>mdi-download</v-icon>
             </v-btn>
           </template>
           <span>Download Kubeconfig</span>
         </v-tooltip>
-      </v-list-item-action>
-      <v-list-item-action class="mx-0" v-if="isKubeconfigAvailable">
-        <copy-btn :clipboard-text="kubeconfig"></copy-btn>
-      </v-list-item-action>
-      <v-list-item-action class="mx-0" v-if="isKubeconfigAvailable">
-        <v-tooltip location="top">
-          <template v-slot:activator="{ on }">
-            <v-btn v-on="on" icon @click.stop="toggleKubeconfig" color="action-button">
-              <v-icon>{{kubeconfigVisibilityIcon}}</v-icon>
+        <copy-btn
+          v-if="isKubeconfigAvailable"
+          :clipboard-text="kubeconfig"
+        />
+        <v-tooltip
+          v-if="isKubeconfigAvailable"
+          location="top"
+        >
+          <template #activator="slotProps">
+            <v-btn
+              icon
+              variant="text"
+              color="action-button"
+              v-bind="slotProps.props"
+              @click.stop="toggleKubeconfig"
+            >
+              <v-icon>{{ kubeconfigVisibilityIcon }}</v-icon>
             </v-btn>
           </template>
-          <span>{{kubeconfigVisibilityTitle}}</span>
+          <span>{{ kubeconfigVisibilityTitle }}</span>
         </v-tooltip>
-      </v-list-item-action>
-      <v-list-item-action v-if="!isGardenloginType" class="mx-0">
-        <static-token-kubeconfig-configuration :shootItem="shootItem"></static-token-kubeconfig-configuration>
-      </v-list-item-action>
+        <static-token-kubeconfig-configuration
+          v-if="!isGardenloginType"
+          :shoot-item="shootItem"
+        />
+      </template>
     </v-list-item>
-    <v-list-item v-if="kubeconfigExpansionPanel" key="expansion-gardenlogin-kubeconfig">
-      <v-list-item-icon></v-list-item-icon>
-      <v-list-item-content class="pt-0">
-        <code-block lang="yaml" :content="kubeconfig" :show-copy-button="false"></code-block>
-      </v-list-item-content>
+    <v-list-item
+      v-if="kubeconfigExpansionPanel"
+      key="expansion-gardenlogin-kubeconfig"
+    >
+      <template #prepend>
+        <v-icon color="primary">
+          blank
+        </v-icon>
+      </template>
+      <code-block
+        lang="yaml"
+        :content="kubeconfig"
+        :show-copy-button="false"
+      />
     </v-list-item>
   </div>
 </template>
 
-<script>
+<script setup>
+import { computed, ref, toRef, watch } from 'vue'
 import CopyBtn from '@/components/CopyBtn.vue'
 import CodeBlock from '@/components/CodeBlock.vue'
 import GardenloginInfo from '@/components/GardenloginInfo.vue'
 import StaticTokenKubeconfigConfiguration from '@/components/StaticTokenKubeconfigConfiguration.vue'
 import download from 'downloadjs'
-import { shootItem } from '@/mixins/shootItem'
+import useShootItem from '@/composables/useShootItem'
 
-export default {
-  components: {
-    CopyBtn,
-    CodeBlock,
-    GardenloginInfo,
-    StaticTokenKubeconfigConfiguration
+const props = defineProps({
+  showListIcon: {
+    type: Boolean,
+    default: false,
   },
-  mixins: [shootItem],
-  props: {
-    showListIcon: {
-      type: Boolean,
-      default: false
-    },
-    type: {
-      type: String,
-      default: 'gardenlogin'
-    }
+  type: {
+    type: String,
+    default: 'gardenlogin',
   },
-  data () {
-    return {
-      kubeconfigExpansionPanel: false
-    }
+  shootItem: {
+    type: Object,
+    required: true,
   },
-  computed: {
-    icon () {
-      return this.showListIcon ? 'mdi-file' : ''
-    },
-    kubeconfig () {
-      return this.isGardenloginType
-        ? this.shootInfo?.kubeconfigGardenlogin
-        : this.shootInfo?.kubeconfig
-    },
-    isKubeconfigAvailable () {
-      return !!this.kubeconfig
-    },
-    kubeconfigVisibilityIcon () {
-      return this.kubeconfigExpansionPanel ? 'mdi-eye-off' : 'mdi-eye'
-    },
-    kubeconfigVisibilityTitle () {
-      return this.kubeconfigExpansionPanel ? 'Hide Kubeconfig' : 'Show Kubeconfig'
-    },
-    isGardenloginType () {
-      return this.type === 'gardenlogin'
-    },
-    getQualifiedName () {
-      const prefix = this.isGardenloginType ? 'kubeconfig-gardenlogin' : 'kubeconfig'
-      return `${prefix}--${this.shootProjectName}--${this.shootName}.yaml`
-    }
-  },
-  methods: {
-    toggleKubeconfig () {
-      this.kubeconfigExpansionPanel = !this.kubeconfigExpansionPanel
-    },
-    reset () {
-      this.kubeconfigExpansionPanel = false
-    },
-    onDownload () {
-      const kubeconfig = this.kubeconfig
-      if (kubeconfig) {
-        download(kubeconfig, this.getQualifiedName, 'text/yaml')
-      }
-    }
-  },
-  watch: {
-    kubeconfig (value) {
-      this.reset()
-    }
+})
+
+const kubeconfigExpansionPanel = ref(false)
+const {
+  shootProjectName,
+  shootName,
+  shootInfo,
+  shootEnableStaticTokenKubeconfig,
+} = useShootItem(toRef(props, 'shootItem'))
+
+const icon = computed(() => props.showListIcon ? 'mdi-file' : '')
+const kubeconfig = computed(() => {
+  const foo = props.shootItem
+  return isGardenloginType.value
+    ? shootInfo.value?.kubeconfigGardenlogin
+    : shootInfo.value?.kubeconfig
+})
+const isKubeconfigAvailable = computed(() => !!kubeconfig.value)
+const kubeconfigVisibilityIcon = computed(() => kubeconfigExpansionPanel.value ? 'mdi-eye-off' : 'mdi-eye')
+const kubeconfigVisibilityTitle = computed(() => {
+  return kubeconfigExpansionPanel.value ? 'Hide Kubeconfig' : 'Show Kubeconfig'
+})
+const isGardenloginType = computed(() => props.type === 'gardenlogin')
+const getQualifiedName = computed(() => {
+  const prefix = isGardenloginType.value ? 'kubeconfig-gardenlogin' : 'kubeconfig'
+  return `${prefix}--${shootProjectName.value}--${shootName.value}.yaml`
+})
+
+const toggleKubeconfig = () => {
+  kubeconfigExpansionPanel.value = !kubeconfigExpansionPanel.value
+}
+const reset = () => {
+  kubeconfigExpansionPanel.value = false
+}
+const onDownload = () => {
+  if (kubeconfig.value) {
+    download(kubeconfig.value, getQualifiedName.value, 'text/yaml')
   }
 }
+
+watch(() => kubeconfig.value, reset)
 </script>
